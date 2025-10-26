2720179a315f49e4d33bc1a73c394dddb93bf5ea
Morteza Masumi <morteza.masumi@gmail.com>
2025-10-26 11:26:14 +0330

    fix: minor

--- resources/views/no-role-widget.blade.php
+++ resources/views/no-role-widget.blade.php
@@ -1,9 +1,7 @@
-@use('\Mortezamasumi\FbEssentials\Facades\FbPersian')
-
 <x-filament-widgets::widget>
     <x-filament::section class="text-lg" icon="heroicon-o-exclamation-triangle" icon-color="warning" icon-size="2xl">
         <div>
-            @lang('fb-user::fb-user.no-role-widget.text', ['hours' => FbPersian::digit(config('fbase.setup.remove_unattend_user_hours', 48))])
+            @lang('fb-user::fb-user.no-role-widget.text', ['hours' => __digit(config('fbase.setup.remove_unattend_user_hours', 48))])
         </div>
     </x-filament::section>
 </x-filament-widgets::widget>
