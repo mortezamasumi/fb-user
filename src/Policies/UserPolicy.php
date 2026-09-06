e6b4ef67db4545391d2691156e338fa9643ecb70
Morteza Masumi <morteza.masumi@gmail.com>
2026-09-06 07:54:26 +0330

    fix: policies has mistake on names

--- src/Policies/UserPolicy.php
+++ src/Policies/UserPolicy.php
@@ -11,76 +11,76 @@ class UserPolicy
 
     public function viewAny(AuthUser $authUser): bool
     {
-        return $authUser->can('view_any_user');
+        return $authUser->can('ViewAny:User');
     }
 
     public function view(AuthUser $authUser): bool
     {
-        return $authUser->can('view_user');
+        return $authUser->can('View:User');
     }
 
     public function create(AuthUser $authUser): bool
     {
-        return $authUser->can('create_user');
+        return $authUser->can('Create:User');
     }
 
     public function update(AuthUser $authUser): bool
     {
-        return $authUser->can('update_user');
+        return $authUser->can('Update:User');
     }
 
     public function delete(AuthUser $authUser): bool
     {
-        return $authUser->can('delete_user');
+        return $authUser->can('Delete:User');
     }
 
     public function deleteAny(AuthUser $authUser): bool
     {
-        return $authUser->can('delete_any_user');
+        return $authUser->can('DeleteAny:User');
     }
 
     public function forceDelete(AuthUser $authUser): bool
     {
-        return $authUser->can('force_delete_user');
+        return $authUser->can('ForceDelete:User');
     }
 
     public function forceDeleteAny(AuthUser $authUser): bool
     {
-        return $authUser->can('force_delete_any_user');
+        return $authUser->can('ForceDeleteAny:User');
     }
 
     public function restore(AuthUser $authUser): bool
     {
-        return $authUser->can('restore_user');
+        return $authUser->can('Restore:User');
     }
 
     public function restoreAny(AuthUser $authUser): bool
     {
-        return $authUser->can('restore_any_user');
+        return $authUser->can('RestoreAny:User');
     }
 
     public function replicate(AuthUser $authUser): bool
     {
-        return $authUser->can('replicate_user');
+        return $authUser->can('Replicate:User');
     }
 
     public function reorder(AuthUser $authUser): bool
     {
-        return $authUser->can('reorder_user');
+        return $authUser->can('Reorder:User');
     }
 
     public function export(AuthUser $authUser): bool
     {
-        return $authUser->can('export_user');
+        return $authUser->can('Export:User');
     }
 
     public function createRoleOnImport(AuthUser $authUser): bool
     {
-        return $authUser->can('create_role_on_import_user');
+        return $authUser->can('CreateRoleOnImport:User');
     }
 
     public function forceChangePassword(AuthUser $authUser): bool
     {
-        return $authUser->can('force_change_password_user');
+        return $authUser->can('ForceChangePassword:User');
     }
 }
